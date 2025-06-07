b55a4c4d4d7d75517e7c69b69b51bc81f46ec81b
keshavjha1310 <keshavjhainft-@atharvacoe.ac.in>
2025-06-07 14:41:09 +0530

    server port update

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const sharp = require('sharp');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.static('public'));
